125e4827cb8adf3bebc5e79cb94df29e056b02e3
njsand <njsand@internode.on.net>
2014-11-13 23:30:02 +1100

    Did p23

--- 21-30.hs
+++ 21-30.hs
@@ -1,3 +1,5 @@
+import System.Random
+
 -- Problems 21 to 30.
 
 -- p21
@@ -5,11 +7,21 @@
 insertAt :: a -> [a] -> Int -> [a]
 insertAt x xs n = let (front, back) = splitAt (n - 1) xs in front ++ (x:back)
 
+-- insertAt' :: a -> [a] -> Int -> [a]
+-- insertAt' x xs n = rec x 
+
+-- NewType DiffList a = DiffList { getDiffList :: ([a] -> [a]) }
+
+-- instance DiffList Monoid where
+--   mempty = DiffList (\x -> x ++ [])
+--   mappend x y = DiffList
+
 -- p22
 -- Create a list of integers in a given range.  Easy!
 range :: Int -> Int -> [Int]
 range x y = [x..y]
 
 -- p23
--- Extract a given number of randomly selected elements from a list. 
--- 
+-- Extract a given number of randomly selected elements from a list.
+rndSelect :: (RandomGen g) => g -> [a] -> Int -> [a]
+rndSelect g xs n = map (xs !!) $ take n $ randomRs (0, length xs - 1) g
