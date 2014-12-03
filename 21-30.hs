15c69125695df2706ccf25de440a1cbf96bbdedd
njsand <njsand@internode.on.net>
2014-12-04 07:23:20 +1100

    doing 24

--- 21-30.hs
+++ 21-30.hs
@@ -1,4 +1,5 @@
 import System.Random
+import Control.Monad(replicateM)
 
 -- Problems 21 to 30.
 
@@ -25,3 +26,38 @@ range x y = [x..y]
 -- Extract a given number of randomly selected elements from a list.
 rndSelect :: (RandomGen g) => g -> [a] -> Int -> [a]
 rndSelect g xs n = map (xs !!) $ take n $ randomRs (0, length xs - 1) g
+
+-- An example of calling this
+rndSelectEx :: (Show a) => [a] -> Int -> IO ()
+rndSelectEx xs n = do
+  gen <- newStdGen
+  putStrLn $ show $ rndSelect gen xs n
+
+rndSelect2 :: [a] -> Int -> IO [a]
+rndSelect2 xs n = do
+  indexes <- replicateM n $ getStdRandom $ randomR (0, length xs - 1)
+  return $ map (xs !!) indexes
+
+-- p24
+-- Lotto: Draw N different random numbers from the set 1..M. 
+-- (So the numbers have to be different.)
+-- lotto :: Int -> Int -> [Int]
+
+-- First define a data structure that allows for a (mostly) compact
+-- representation of a function that is basically \x -> x but has some
+-- discontinuities/steps in it.
+addStep :: (Ord a, Num a) => [(a, a)] -> a -> [(a, a)]
+addStep [] y = [(y, y)]
+addStep all@(range@(x1, x2):xs) y
+  | inRange range y = all
+  | y == x1 - 1 = (y, x2):xs
+  | y < x1 = (y, y):all
+  | y == x2 + 1 = (x1, y):xs
+  | y > x2 = range : (addStep xs y)
+
+inRange :: (Ord a) => (a, a) -> a -> Bool
+inRange (x, y) z = z >= x && z <= y
+
+index (Ord a, Num a) => [(a, a)] -> Int -> a
+index [] x = x
+index []
